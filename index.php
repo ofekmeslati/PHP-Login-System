55cfd831a125a8ee092218a2df1c2999235201b3
Ofek Meslati <ofekmaslaty01@gmail.com>
2023-07-30 18:10:49 +0100

    Added config; added login/reg pages; added footer

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php 
+// allow the config
+define('__CONFIG__', true);
+// require the config
+require_once "inc/config.php";
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -15,37 +23,16 @@
   <body>
 
   	<div class="uk-section uk-container">
-  		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
-			<form class="uk-form-stacked js-login">
-
-			    <div class="uk-margin">
-			        <label class="uk-form-label" for="form-stacked-text">Email</label>
-			        <div class="uk-form-controls">
-			            <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
-			        </div>
-			    </div>
-
-			    <div class="uk-margin">
-			        <label class="uk-form-label" for="form-stacked-text">Password</label>
-			        <div class="uk-form-controls">
-			            <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your Password">
-			        </div>
-			    </div>
-
-			    <div class="uk-margin">
-			        <button class="uk-button uk-button-default" type="submit">Login</button>
-			    </div>
-
-			</form>
-  		</div>
+    <?php
+        echo "Hello world the date is: ";
+        echo date("Y M D");
+        echo "<br />";
+    ?>
+    <p><a href="/php_login_project/login.php">Login</a></p>
+    <p><a href="/php_login_project/register.php">Register</a></p>
   	</div>
 
-    <!-- jQuery is required -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-    <!-- UIkit JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit-icons.min.js"></script>
+<?php require_once "inc/footer.php"; ?>
 
   </body>
 </html>
